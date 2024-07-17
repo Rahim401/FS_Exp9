b14e6a4b11785e7d24dca3fd7aaabbc40660eb4c
Rahim401 <hrahim401@gmail.com>
2024-07-17 07:28:50 +0530

    Initial Commit

---
+++ StudentApp/forms.py
@@ -0,0 +1,6 @@
+from django import forms
+from .models import Project
+class ProjectForm(forms.ModelForm):
+	class Meta:
+		model = Project
+		fields = ['student_name', 'topic_chosen', 'languages_used', 'duration']
